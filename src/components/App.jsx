cb3245f52289a8d8db4668adf11da85baf388c1e
LifeLong-LeArneR17 <118177362+LifeLong-LeArneR17@users.noreply.github.com>
2023-03-13 16:47:47 +0200

    fix
    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,77 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { refreshUser } from 'redux/user/userOperations';
+import { PrivateRoute } from './PrivateRoute';
+import { RestrictedRoute } from './RestrictedRoute';
+import { useAuth } from 'hook/useAuth';
+import { Loader } from './Loader/Loader';
+import React, { lazy, Suspense } from 'react';
+
+const HomePage = lazy(() => import('pages/HomePage/HomePage'));
+const ReportsPage = lazy(() => import('pages/ReportsPage/ReportsPage'));
+const LoginPage = lazy(() => import('pages/LoginPage/LoginPage'));
+const RegisterPage = lazy(() => import('pages/RegisterPage/RegisterPage'));
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+  const { isRefreshing } = useAuth();
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <Loader />
+  ) : (
+    <>
+      <Suspense falback={<Loader />}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />}>
+            <Route
+              path="/expenses"
+              element={
+                <PrivateRoute redirectTo="/login" component={<Expenses />} />
+              }
+            />
+            <Route
+              path="/income"
+              element={
+                <PrivateRoute redirectTo="/login" component={<Income />} />
+              }
+            />
+
+            <Route
+              path="/login"
+              element={
+                <RestrictedRoute
+                  redirectTo="/expenses"
+                  component={<LoginPage />}
+                />
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <RestrictedRoute
+                  redirectTo="/expenses"
+                  component={<RegisterPage />}
+                />
+              }
+            />
+
+            <Route
+              path="/transaction/period-data"
+              element={
+                <PrivateRoute redirectTo="/login" component={<ReportsPage />} />
+              }
+            />
+          </Route>
+
+          <Route path="*" element={<Navigate to="/" replace={true} />} />
+        </Routes>
+      </Suspense>
+    </>
   );
 };
